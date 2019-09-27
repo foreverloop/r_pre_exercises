51750628aeba1f28d885588b768e72f8e3ac04ac
foreverloop <cjones5709@gmail.com>
2019-09-27 18:52:18 +0100

    started work

--- valid_tic_tac.R
+++ valid_tic_tac.R
@@ -1,1 +1,15 @@
 #naughts and crosses validation through linear algebra
+
+mat_horizontal <- matrix(data=c(1,0,0,1,0,0,1,0,0),nrow=3,ncol=3)
+mat_horizontal
+
+mat_vertical <- matrix(data=c(1,1,1,0,0,0,0,0,0),nrow=3,ncol=3)
+mat_vertical
+
+mat_cross <- matrix(data=c(1,0,0,0,1,0,0,0,1),nrow=3,ncol=3)
+mat_cross
+
+help(sapply)
+sapply(mat_horizontal,FUN=function(x) {return(x +1)})
+
+print(check_horizontal(mat_horizontal))
