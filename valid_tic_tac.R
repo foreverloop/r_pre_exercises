89556e6734ad7d023d930038cd8ea513ccbd7d34
foreverloop <cjones5709@gmail.com>
2019-09-29 12:17:14 +0100

    added vertical and horizontal checks:

--- valid_tic_tac.R
+++ valid_tic_tac.R
@@ -1,6 +1,9 @@
 #naughts and crosses validation through linear algebra
 #git push attempt halls
-mat_horizontal <- matrix(data=c(1,0,0,1,0,0,1,0,0),nrow=3,ncol=3)
+#mat_horizontal <- matrix(data=c(1,0,0,1,0,0,1,0,0),nrow=3,ncol=3)
+#mat_horizontal
+
+mat_horizontal <- matrix(data=c(1,1,1,0,0,0,0,0,0),nrow=3,ncol=3,byrow=TRUE)
 mat_horizontal
 
 mat_vertical <- matrix(data=c(1,1,1,0,0,0,0,0,0),nrow=3,ncol=3)
@@ -9,7 +12,22 @@ mat_vertical
 mat_cross <- matrix(data=c(1,0,0,0,1,0,0,0,1),nrow=3,ncol=3)
 mat_cross
 
-help(sapply)
-sapply(mat_horizontal,FUN=function(x) {return(x +1)})
+c(mat_horizontal)
+c(t(mat_vertical))
+c(mat_cross)
 
-print(check_horizontal(mat_horizontal))
+#apply lambda function to each row in matrix
+apply(mat_horizontal, FUN=function(x) {
+  if (length(union(c(1),x)) == 1 && (x[1][1] != 0))
+    return(TRUE)
+  return(FALSE)
+  },MARGIN=c(0,1))
+
+#transpose the matrix to check for vertical wins
+apply(t(mat_vertical), FUN=function(x) {
+  print(x)
+  if (length(union(c(1),x)) == 1 && (x[1][1] != 0))
+    return(TRUE)
+  return(FALSE)
+},MARGIN=c(0,1))
+
