f7c3ec53a69f948cca9c9ebcad2b2548c01dc99d
foreverloop <cjones5709@gmail.com>
2019-09-29 14:49:00 +0100

    added diagonal checking

--- valid_tic_tac.R
+++ valid_tic_tac.R
@@ -1,20 +1,9 @@
 #naughts and crosses validation through linear algebra
-#git push attempt halls
-#mat_horizontal <- matrix(data=c(1,0,0,1,0,0,1,0,0),nrow=3,ncol=3)
-#mat_horizontal
 
 mat_horizontal <- matrix(data=c(1,1,1,0,0,0,0,0,0),nrow=3,ncol=3,byrow=TRUE)
-mat_horizontal
-
 mat_vertical <- matrix(data=c(1,1,1,0,0,0,0,0,0),nrow=3,ncol=3)
-mat_vertical
-
 mat_cross <- matrix(data=c(1,0,0,0,1,0,0,0,1),nrow=3,ncol=3)
-mat_cross
-
-c(mat_horizontal)
-c(t(mat_vertical))
-c(mat_cross)
+mat_cross_min <- matrix(data=c(0,0,1,0,1,0,1,0,0),nrow=3,ncol=3)
 
 #apply lambda function to each row in matrix
 apply(mat_horizontal, FUN=function(x) {
@@ -25,9 +14,21 @@ apply(mat_horizontal, FUN=function(x) {
 
 #transpose the matrix to check for vertical wins
 apply(t(mat_vertical), FUN=function(x) {
-  print(x)
   if (length(union(c(1),x)) == 1 && (x[1][1] != 0))
     return(TRUE)
   return(FALSE)
 },MARGIN=c(0,1))
 
+#check for both possible diagonal wins
+check_diagonal <- function(game_matrix){
+  if(length(union(c(1),c(game_matrix[1,1],game_matrix[2,2],game_matrix[3,3]))) == 1){
+    return(TRUE)
+  }
+  if(length(union(c(1),c(game_matrix[3,1],game_matrix[2,2],game_matrix[1,3]))) == 1){
+    return(TRUE)
+  }
+  return(FALSE)
+}
+
+check_diagonal(mat_cross)
+check_diagonal(mat_cross_min)
